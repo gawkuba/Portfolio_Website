6bdc4261bd4c364ac6e933993f976ac3479c3a2f
Jakub Gaweł <gawkuba@gmail.com>
2023-08-31 19:07:40 +0200

    commiting

---
+++ 1landing_page/javascript/opening_page.js
@@ -0,0 +1,105 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const flexbox1_item1 = document.querySelector(".flexbox-item-1");
+    const flexbox1_item2 = document.querySelector(".flexbox-item-2");
+    const flexbox1_item3 = document.querySelector(".flexbox-item-3");
+
+    const flexbox2_item1 = document.querySelector(".flexbox2-item-1");
+    const flexbox2_item2 = document.querySelector(".flexbox2-item-2");
+    const flexbox2_item3 = document.querySelector(".flexbox2-item-3");
+
+    flexbox1_item1.addEventListener("mouseenter", function () {
+        flexbox1_item2.style.opacity = "0.1";
+        flexbox1_item3.style.opacity = "0.1";
+        flexbox2_item1.style.opacity = "0.1";
+        flexbox2_item2.style.opacity = "0.1";
+        flexbox2_item3.style.opacity = "0.1";
+    });
+
+    flexbox1_item1.addEventListener("mouseleave", function () {
+        flexbox1_item2.style.opacity = "1";
+        flexbox1_item3.style.opacity = "1";
+        flexbox2_item1.style.opacity = "1";
+        flexbox2_item2.style.opacity = "1";
+        flexbox2_item3.style.opacity = "1";
+    });
+
+    flexbox1_item2.addEventListener("mouseenter", function () {
+        flexbox1_item1.style.opacity = "0.1";
+        flexbox1_item3.style.opacity = "0.1";
+        flexbox2_item1.style.opacity = "0.1";
+        flexbox2_item2.style.opacity = "0.1";
+        flexbox2_item3.style.opacity = "0.1";
+    });
+
+    flexbox1_item2.addEventListener("mouseleave", function () {
+        flexbox1_item1.style.opacity = "1";
+        flexbox1_item3.style.opacity = "1";
+        flexbox2_item1.style.opacity = "1";
+        flexbox2_item2.style.opacity = "1";
+        flexbox2_item3.style.opacity = "1";
+    });
+
+    flexbox1_item3.addEventListener("mouseenter", function () {
+        flexbox1_item2.style.opacity = "0.1";
+        flexbox1_item1.style.opacity = "0.1";
+        flexbox2_item1.style.opacity = "0.1";
+        flexbox2_item2.style.opacity = "0.1";
+        flexbox2_item3.style.opacity = "0.1";
+    });
+
+    flexbox1_item3.addEventListener("mouseleave", function () {
+        flexbox1_item2.style.opacity = "1";
+        flexbox1_item1.style.opacity = "1";
+        flexbox2_item1.style.opacity = "1";
+        flexbox2_item2.style.opacity = "1";
+        flexbox2_item3.style.opacity = "1";
+    });
+
+    flexbox2_item1.addEventListener("mouseenter", function () {
+        flexbox1_item1.style.opacity = "0.1";
+        flexbox1_item2.style.opacity = "0.1";
+        flexbox1_item3.style.opacity = "0.1";
+        flexbox2_item2.style.opacity = "0.1";
+        flexbox2_item3.style.opacity = "0.1";
+    });
+
+    flexbox2_item1.addEventListener("mouseleave", function () {
+        flexbox1_item1.style.opacity = "1";
+        flexbox1_item2.style.opacity = "1";
+        flexbox1_item3.style.opacity = "1";
+        flexbox2_item2.style.opacity = "1";
+        flexbox2_item3.style.opacity = "1";
+    });
+
+    flexbox2_item2.addEventListener("mouseenter", function () {
+        flexbox1_item1.style.opacity = "0.1";
+        flexbox1_item2.style.opacity = "0.1";
+        flexbox1_item3.style.opacity = "0.1";
+        flexbox2_item1.style.opacity = "0.1";
+        flexbox2_item3.style.opacity = "0.1";
+    });
+
+    flexbox2_item2.addEventListener("mouseleave", function () {
+        flexbox1_item1.style.opacity = "1";
+        flexbox1_item2.style.opacity = "1";
+        flexbox1_item3.style.opacity = "1";
+        flexbox2_item1.style.opacity = "1";
+        flexbox2_item3.style.opacity = "1";
+    });
+
+    flexbox2_item3.addEventListener("mouseenter", function () {
+        flexbox1_item1.style.opacity = "0.1";
+        flexbox1_item2.style.opacity = "0.1";
+        flexbox1_item3.style.opacity = "0.1";
+        flexbox2_item1.style.opacity = "0.1";
+        flexbox2_item2.style.opacity = "0.1";
+    });
+
+    flexbox2_item3.addEventListener("mouseleave", function () {
+        flexbox1_item1.style.opacity = "1";
+        flexbox1_item2.style.opacity = "1";
+        flexbox1_item3.style.opacity = "1";
+        flexbox2_item1.style.opacity = "1";
+        flexbox2_item2.style.opacity = "1";
+    });
+});
